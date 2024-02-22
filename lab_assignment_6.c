9416e932bf9b20b64f299db678547393692d797a
Elias Wainberg <elias.wainberg@ucf.edu>
2024-02-22 12:11:52 -0500

    Adds search function

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
-int search(int numbers[], int low, int high, int value) 
+int search(int numbers[], int low, int high, int value)
 {
-	return -1;
+    if (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (numbers[mid] == value)
+        {
+            return mid; // Value found at index mid
+        }
+        else if (numbers[mid] < value)
+        {
+            return search(numbers, mid + 1, high, value); // Search the right half
+        }
+        else
+        {
+            return search(numbers, low, mid - 1, value); // Search the left half
+        }
+    }
+
+    return -1; // Value not found
 }
 
 void printArray(int numbers[], int sz)
@@ -28,7 +46,7 @@ int main(void)
 	FILE* inFile = fopen("input.txt","r");
 
 	fscanf(inFile, " %d\n", &numInputs);
-	
+
 	while (numInputs-- > 0)
 	{
 		fscanf(inFile, " %d\n", &countOfNums);
